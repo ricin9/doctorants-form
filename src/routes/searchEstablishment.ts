1d8c9d6fa9065f24f8feb977e0c6e3a9fb04a238
ricin9 <ricin9@outlook.com>
2023-09-21 12:00:11 +0100

    forgor to add untracked files

---
+++ src/routes/searchEstablishment.ts
@@ -0,0 +1,11 @@
+import { establishmentOptions } from '$lib/common/formEnums';
+
+export async function searchEstablishment(query: string) {
+	const establishments = establishmentOptions;
+	if (query === '') {
+		return establishments;
+	}
+	return establishments.filter((establishment) =>
+		establishment.label.toLocaleLowerCase().includes(query)
+	);
+}
