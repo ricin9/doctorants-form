917cdf4e24007ed3050262a60f16b9504971adca
ricin9 <ricin9@outlook.com>
2023-09-24 14:26:07 +0100

    fixed origin error

--- src/routes/reinscription/scanned-document/+server.ts
+++ src/routes/reinscription/scanned-document/+server.ts
@@ -1,6 +1,7 @@
 import { fail, redirect } from '@sveltejs/kit';
 import { getUserReinscription } from '../../getUserReinscription';
 import { readFile } from 'fs/promises';
+import { UPLOADED_FILE_DEST } from '$env/static/private';
 
 export async function GET({ locals }) {
 	if (!locals.session) {
@@ -12,5 +13,6 @@ export async function GET({ locals }) {
 		throw fail(404, { message: 'file not found' });
 	}
 
-	return new Response(Buffer.from(await readFile(reg.file)));
+	const filePath = `${UPLOADED_FILE_DEST}/${reg.file}`;
+	return new Response(Buffer.from(await readFile(filePath)));
 }
